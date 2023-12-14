da7941f7c3fbb3308cd80d65972aefe3dfbf7523
FenomananaN <fenomanana.nomenjanahary@gmail.com>
2023-12-14 05:07:49 -0800

    fix token ownable

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,11 +1,36 @@
-// contracts/GLDToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract Girefy is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Girefy", "GRF") {
-        _mint(msg.sender, initialSupply);
+contract Girefy is ERC20, ERC20Pausable, Ownable, ERC20Permit {
+    constructor()
+        ERC20("Girefy", "GRF")
+        Ownable(msg.sender)
+        ERC20Permit("Girefy")
+    {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner{
+        _mint(to, amount*(10**18));
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
     }
 }
